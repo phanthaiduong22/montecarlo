926c50c10ac18ece9964d8074853fdb3ee38dbd8
DuongPhan <phanthaiduong2000@gmail.com>
2020-09-29 20:40:31 +0700

    add airthmetic formula

--- montecarlo.py
+++ montecarlo.py
@@ -1,19 +1,37 @@
 from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
 import numpy as np
-<< << << < Updated upstream
-%matplotlib notebook
+# %matplotlib notebook
+
+test = []
+pi_list = []
+n_circle = 0
+n_square = 0
+num = 100000
+R = 1
 theta = np.linspace(0, 2*np.pi, 100)
 
-for i in range(100):
-    test.append([np.random.uniform(-1.0, 1.0), np.random.uniform(-1.0, 1.0)])
+
+for i in range(num):
+    x = np.random.uniform(-1.0, 1.0)
+    y = np.random.uniform(-1.0, 1.0)
+    dis = x*x + y*y - R*R
+    if(dis <= 0):
+        n_circle += 1
+    else:
+        n_square += 1
+    pi = (4 * n_circle) / ((n_circle + n_square) * R*R)
+    pi_list.append(pi)
+    test.append([x, y])
 
 r = np.sqrt(1.0)
 
 
 def animate(i):
     ax.scatter(test[i][0], test[i][1], 10, color='red')
-    #graph.set_data(test[i][0], test[i][1])
+    plt.text(0.3, 1.05, "pi = {}".format(pi_list[i]),
+             transform=plt.gca().transAxes)
+    # graph.set_data(test[i][0], test[i][1])
     # return graph
 
 
@@ -29,14 +47,3 @@ plt.ylim(-1, 1)
 plt.grid(linestyle='--')
 ani = FuncAnimation(fig, animate, frames=1000, interval=200)
 plt.show()
-== == == =
-
-circle = plt.Circle((0.5, 0.5), 0.5, color='blue')
-
-fig, ax = plt.subplots()  # note we must use plt.subplots, not plt.subplot
-
-plt.plot([0.5], [0.5], marker='o', markersize=3, color="red")
-ax.add_artist(circle)
-
-plt.show()
->>>>>> > Stashed changes
--- test.py
+++
@@ -1,7 +0,0 @@
-draw_circle = plt.Circle((0.5, 0.5), 0.3)
-
-axes.set_aspect(1)
-axes.plot([0.5], [0.5], 'o')
-axes.add_artist(draw_circle)
-plt.title('Circle')
-plt.show()
