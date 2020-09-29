e9adeaba8d744ad09562a6fa4889c673b6d07f11
hedwig <phanthaiduong22>
2020-09-29 18:03:26 +0700

    first commit

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+ax.annotate('a polar annotation',
+#             xy=(thistheta, thisr),  # theta, radius
+#             xytext=(0.5, 0.5),    # fraction, fraction
+#             textcoords='figure fraction',
+#             arrowprops=dict(facecolor='black', shrink=0.05),
+#             horizontalalignment='left',
+#             verticalalignment='bottom',
+#             )
---
+++ test.py
@@ -0,0 +1,5 @@
+from matplotlib.pyplot import figure
+figure(num=None, figsize=(8, 6), dpi=80, facecolor='w', edgecolor='k')
+
+plt.title('Circle')
+plt.show()
