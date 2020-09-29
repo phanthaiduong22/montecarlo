9f6d90bfe3fff94a79c378f4a993e9e4072103a7
lvu5 <50764990+lvu5@users.noreply.github.com>
2020-09-29 18:19:10 +0700

    Update montecarlo.py

--- montecarlo.py
+++ montecarlo.py
@@ -1,12 +1,18 @@
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure
 
-f, axes = plt.subplots()
-figure(num=None, figsize=(8, 6), dpi=80, facecolor='w', edgecolor='k')
-draw_circle = plt.Circle((0.5, 0.5), 0.3)
+theta = np.linspace(0, 2*np.pi, 100)
 
-axes.set_aspect(1)
-axes.plot([0.5], [0.5], 'o')
-axes.add_artist(draw_circle)
-plt.title('Circle')
+r = np.sqrt(1.0)
+
+x1 = r*np.cos(theta)
+x2 = r*np.sin(theta)
+fig, ax = plt.subplots(1)
+
+ax.plot(x1, x2)
+ax.set_aspect(1)
+graph, = plt.plot([], [], 'o')
+plt.xlim(-1,1)
+plt.ylim(-1,1)
+plt.grid(linestyle='--')
 plt.show()
